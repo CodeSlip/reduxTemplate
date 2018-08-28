897271b49fac8140c27823df7bb9c06f7b362901
CodeSlip <jason@rawlings.io>
2018-08-28 17:27:59 -0400

    streamlined redux implementation

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,4 +1,5 @@
-import * as actionTypes from '../actions';
+import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from './utility';
 
 const initialState = {
     counter: 0
@@ -7,23 +8,16 @@ const initialState = {
 const counterReducer = (state = initialState, action) => {
     switch ( action.type ) {
         case actionTypes.INCREMENT:
-            return {
-                ...state,
-                counter: state.counter + 1,
-            }
+            return updateObject({state, counter: state.counter + 1});
         case actionTypes.DECREMENT:
-            return {
-                ...state,
-                counter: state.counter - 1,
-        }
+            return updateObject({state, counter: state.counter - 1});        
         case actionTypes.ADD:
-            return {
-                ...state,
-                counter: state.counter + action.val
-            }
-        }
-    return state;    
+            return updateObject({state, counter: state.counter + 10});        
+        case actionTypes.SUBTRACT:
+            return updateObject({state, counter: state.counter - 10});
+        default:
+            return state
+        }    
 }
-
 
 export default counterReducer;
